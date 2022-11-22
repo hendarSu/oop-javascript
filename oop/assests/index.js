5208ab0d85608eb9d5b26277da14218379338496
Hendar <hendarsundana@gmail.com>
2022-11-22 22:21:13 +0700

    add example encapsulation

--- oop/assests/index.js
+++ oop/assests/index.js
@@ -11,13 +11,13 @@ class Book {
         return this;
     }
 
-    static setTableRow() {
+    static #setTableRow() {
         LIST_OF_BOOK.forEach((data) => {
             $("#table-book tbody").append(`<tr><td>${data.title}</td><td>${data.author}</td><td>${data.year}</td><td>${(data.category) ? data.category : "-"}</td></tr>`);
         });
     }
     
-    static find(title) {
+    static _find(title) {
         $("#table-book tbody").empty();
         console.log(title);
         if (title) {
@@ -31,30 +31,58 @@ class Book {
                 $("#table-book tbody").append(`<tr><td colspan="4">Buku <b>${title}</b> tidak ditemukan!</td></tr>`);
             }
         } else {
-            this.setTableRow();
+            this.#setTableRow();
         }
     }
 }
 
-// Disini kita akan implementasi kelas turunan dari Book.
-class BiografiBook extends Book {
-    constructor(title, author, year) {
-        super(title, author, year);
-    }
 
+const GenerateCategory = Base => class extends Base {
     setCategory(category) {
         this.category = category;
     }
 }
+// Disini kita akan implementasi kelas turunan dari Book.
+class BiografiBook extends GenerateCategory(Book) {
+    constructor(title, author, year) {
+        super(title, author, year);
+    }
+}
 
-class NovelBook extends Book {
+class BiografiBook2 {
+    #setTitle(title) {
+        this.title = title;
+    }
+
+    #setAuthor(author) {
+        this.author = author;
+    }
+
+    #setYear(year) {
+        this.year = year;
+    }
+
+    setBook(title, author, year) {
+        this.#setAuthor(author);
+        this.#setTitle(title);
+        this.#setYear(year);
+    }
+
+    addBook() {
+        LIST_OF_BOOK.push(this);
+    }
+}
+
+class NovelBook extends GenerateCategory(Book) {
     constructor(title, author, year) {
         super(title, author, year);
     }
 }
 
 const novel_1 = new NovelBook("Rindu", "Tere Liye", "2009");
-LIST_OF_BOOK.push(novel_1.getBook());
+novel_1.setCategory("Novel");
+
+LIST_OF_BOOK.push(novel_1.getBook())
 
 const novel_2 = new NovelBook("Si Putih", "Tere Liye", "2004");
 LIST_OF_BOOK.push(novel_2.getBook());
@@ -74,6 +102,10 @@ const biografi_3 = new BiografiBook("Sejarah Tuhan", "Karen Armstrong", "1993");
 biografi_3.setCategory("Sejarah");
 LIST_OF_BOOK.push(biografi_3.getBook());
 
+const biografi_4 = new BiografiBook2();
+biografi_4.setBook("Testing book 1", "Hendar", "2022");
+biografi_4.addBook();
+
 LIST_OF_BOOK.forEach((data) => {
     $("#table-book tbody").append(`<tr><td>${data.title}</td><td>${data.author}</td><td>${data.year}</td><td>${(data.category) ? data.category : "-"}</td></tr>`);
 })
@@ -81,7 +113,7 @@ LIST_OF_BOOK.forEach((data) => {
 $(document).ready(() => {
     $("input#input-search-book").keyup(function () {
         const name = $("input#input-search-book").val();
-        Book.find(name);
+        Book._find(name);
     });
 })
 
